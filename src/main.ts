ee76ca64cb14137c52ce187dbdbcfac37e6a90ce
Tuan Phan (u810057) <tuan.phan@mobi.ch>
2023-04-05 13:29:21 +0700

    feat: migrate to standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ProductService } from './app/demo/service/product.service';
+import { PhotoService } from './app/demo/service/photo.service';
+import { NodeService } from './app/demo/service/node.service';
+import { IconService } from './app/demo/service/icon.service';
+import { EventService } from './app/demo/service/event.service';
+import { CustomerService } from './app/demo/service/customer.service';
+import { CountryService } from './app/demo/service/country.service';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {provideHttpClient} from "@angular/common/http";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(AppRoutingModule, BrowserAnimationsModule),
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        CountryService, CustomerService, EventService, IconService, NodeService,
+        PhotoService, ProductService, provideHttpClient()
+    ]
+})
   .catch(err => console.error(err));
